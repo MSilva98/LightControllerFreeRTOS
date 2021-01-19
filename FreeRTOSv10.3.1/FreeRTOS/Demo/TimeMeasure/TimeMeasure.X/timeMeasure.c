d6228d519f51161532596bba0b71216757b9e2a5
Marco Silva <masilva1998@hotmail.com>
2021-01-19 10:14:21 +0000

    Time conversion to ms

--- FreeRTOSv10.3.1/FreeRTOS/Demo/TimeMeasure/TimeMeasure.X/timeMeasure.c
+++ FreeRTOSv10.3.1/FreeRTOS/Demo/TimeMeasure/TimeMeasure.X/timeMeasure.c
@@ -5,13 +5,10 @@
 
 #include <xc.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <stdio.h>
-#include <time.h>
-
  
-#define BILLION  1000000000.0
-
+#define NR_ADC_SAMPLES 5
+    
 /* Prototypes for internal functions */
 void termInit(void);
 
@@ -41,90 +38,55 @@ int main(int argc, char** argv)
     IFS0bits.T2IF = 0;
     IFS0bits.T3IF = 0; 
     
-    uint32_t n = 100;       // NUMBER OF RUNS
+    uint32_t n = 200;       // NUMBER OF RUNS
     double vals[n];
     uint32_t tmrs[n];
     uint32_t i = 0;
     char msg[200];
     
-    sprintf(msg, "TIMER CONFIGURATION COMPLETE\n\r");
-    PrintStr(msg);
+    // ADC config.
+    AD1CON1bits.SSRC=7; // Internal counter ends sampling and starts conversion
+    AD1CON1bits.CLRASAM=1; //Stop conversion when 1st A/D converter interrupt is
+    // generated and clear ASAM bit automatically
+    AD1CON1bits.FORM=0; // Integer 16 bit output format
+    AD1CON2bits.VCFG=0; // VR+=AVdd; VR-=AVss
+    AD1CON2bits.SMPI=0; // Number (+1) of consecutive coversions, stored in buffer
+    AD1CON3bits.ADRC=1; // ADC uses internal RC clock
+    AD1CON3bits.SAMC=16; // Sample time is 16TAD ( TAD = 100ns)
+    // Set AN0 as input
+    AD1CHSbits.CH0SA=0; // Select AN0 as input for A/D converter
+    TRISBbits.TRISB0=1; // AN0 in input mode
+    AD1PCFGbits.PCFG0=0; // AN0 as analog input
+    // Enable module
+    AD1CON1bits.ON = 1; // Enable A/D module
+    
+    uint8_t j = 0;
+    uint32_t ldr_values[NR_ADC_SAMPLES]; //valores que vem do LDR / ADC
     
     for(i = 0; i < n; i++){
         PORTCbits.RC1 = 1;
         T2CONbits.TON = 1;
-        // START EXECUTION TIME MEASURE
         
-        sprintf(msg, "TESTING EXECUTION TIME\n\r");
-        PrintStr(msg);
-        double d = 247821.312;
-        double x = 34252.155;
-        double y = 1425.2342;
+        //------------------------------\\
+        // START EXECUTION TIME MEASURE \\
+        //------------------------------\\
         
-        double t = d*x;
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
-        t = x*x*x*y;
-        sprintf(msg, "RESULT %f\n\r", t);
-        PrintStr(msg);
+        IFS1bits.AD1IF = 0; // Reset interrupt flag
+        AD1CON1bits.ASAM=1; // Start conversion
+        while(IFS1bits.AD1IF == 0); // Wait for EOC
+        ldr_values[j] = ADC1BUF0;
+        j++;
+        if(j>NR_ADC_SAMPLES)
+            j = 0;
         
-        // END EXECUTION TIME MEASURE
+        //------------------------------\\        
+        // END EXECUTION TIME MEASURE --\\
+        //------------------------------\\
+
         T2CONbits.ON = 0;
         T3CONbits.ON = 0;
         
-        vals[i] = (double)TMR2/(double)PBCLOCK;
+        vals[i] = ((double)TMR2/(double)PBCLOCK)/25.0;
         tmrs[i] = TMR2;
         sprintf(msg,"Iteration: %d TMR2: %d Time: %f\n\r", i, TMR2, vals[i]);
         PrintStr(msg);
@@ -142,7 +104,7 @@ int main(int argc, char** argv)
         mean += vals[i];
     }
     
-    sprintf(msg, "Mean execution time: %f\n\r", mean/(double)n);
+    sprintf(msg, "Mean execution time: %.10f ms\n\r", (mean*1000000)/(double)n);
     PrintStr(msg);
     UartClose();
 
